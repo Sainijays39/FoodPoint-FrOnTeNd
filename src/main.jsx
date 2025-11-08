b4ff80cf0092eb9ffea03f9c49836b6c9eb1334b
sainijays <sainijays39@gmail.com>
2025-11-08 17:02:40 +0530

    add some upi debug console..

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,18 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
+import { App as CapacitorApp } from '@capacitor/app';
+
+
+CapacitorApp.addListener('appUrlOpen', (data) => {
+  const url = data.url;
+  console.log('Deep link received:', url);
+  if (url.includes('/reset-password')) {
+    const token = url.split('/reset-password/')[1];
+    // Redirect inside your app
+    window.location.href = `/reset-password/${token}`;
+  }
+});
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
